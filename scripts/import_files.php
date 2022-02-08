2a5444a9578775a8737926b78a6933b7ec1a2518
Brian Rak <dn@devicenull.org>
2022-02-08 17:34:54 -0500

    Add ems meeting type, stop trying to embed doc files

--- scripts/import_files.php
+++ scripts/import_files.php
@@ -48,6 +48,7 @@ foreach (getDirContents(__DIR__.'/../web/files') as $cur)
 		case 'planning':
 		case 'zoning':
 		case 're-warding':
+		case 'ems':
 			if (!preg_match_all('/^[0-9]+\\-[0-9]+\\-[0-9]+$/i', $date))
 			{
 				echo "Invalid file format: $cleanpath\n";
