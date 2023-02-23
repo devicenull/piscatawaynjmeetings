93aac264d54d06bd929461d7f57f384dd004f2ea
Brian Rak <dn@devicenull.org>
2023-02-22 19:21:29 -0500

    Fix path issue

--- web/sitemap.php
+++ web/sitemap.php
@@ -34,7 +34,7 @@ foreach (Meeting::getAll() as $meeting)
 	}
 	if ($meeting['transcript_available'] == 'yes')
 	{
-		$sitemap->addEntry(BASEURL.'/meeting.php?MEETINGID='.$meeting['MEETINGID'], $meeting['last_updated']);
+		$sitemap->addEntry(BASEURL.'/transcript.php?MEETINGID='.$meeting['MEETINGID'], $meeting['last_updated']);
 		$sitemap->addEntry(BASEURL.$meeting->getLink('transcript'), $meeting['last_updated']);
 	}
 }
