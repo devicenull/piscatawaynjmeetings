2d5ae9255aac5fda0195e8a51020cc35107ffafc
Brian Rak <dn@devicenull.org>
2023-02-22 20:19:11 -0500

    Add initial structured data

--- web/transcript.php
+++ web/transcript.php
@@ -1,6 +1,18 @@
 <?php
 require(__DIR__.'/../init.php');
+$meeting = new Meeting(['MEETINGID' => $_GET['MEETINGID']]);
 $vars = [
-	'meeting' => new Meeting(['MEETINGID' => $_GET['MEETINGID']]),
+	'meeting' => $meeting,
+	'json_ld' => json_encode([
+		'@context'            => 'https://schema.org',
+		'@type'               => 'Event',
+		'name'                => 'Piscataway, New Jersey '.ucfirst($meeting['type']).' Meeting',
+		'startDate'           => $meeting['date'],
+		'eventAttendanceMode' => 'https://schema.org/OnlineEventAttendanceMode',
+		'location'            => [
+			'@type' => 'VirtualLocation',
+			'url'   => ($meeting['zoom_id'] != '') ? 'zoommtg://zoom.us/join?confno='.$meeting['zoom_id'].'&pwd='.sprintf("%06s", $meeting['zoom_password']) : '',
+		],
+	]),
 ];
 displayPage('transcript.html', $vars);
